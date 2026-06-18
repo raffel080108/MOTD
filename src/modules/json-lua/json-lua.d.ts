d06b23e79db33fd9c24fccc7f9b26be33066dbb6
raffel080108 <raffel080108@outlook.de>
2026-06-19 00:15:28 +0200

    Deleted json-lua files since they are not used for the UE4SS-L version (I forgot oops)

--- src/modules/json-lua/json-lua.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Encodes an object as a json string
- */
-export function encode(val: unknown): string;
-
-/**
- * Decodes a json string to an object
- */
-export function decode(tr: string): unknown;
